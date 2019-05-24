4124a1fac503afa40b6cf53c3fbcf090b0fd06bd
Simon Martinelli <simon@72.services>
2019-05-24 13:29:51 +0200

    initial commit

---
+++ src/main/java/io/seventytwo/demo/jooqemployee/JooqEmployeeApplication.java
@@ -0,0 +1,13 @@
+package io.seventytwo.demo.jooqemployee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JooqEmployeeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JooqEmployeeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/seventytwo/demo/jooqemployee/JooqEmployeeApplicationTests.java
@@ -0,0 +1,16 @@
+package io.seventytwo.demo.jooqemployee;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class JooqEmployeeApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
